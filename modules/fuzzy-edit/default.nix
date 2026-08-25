1df2c273c2d64742816099aeeafc42b8e65abebe
heureka-code <>
2026-08-25 18:14:44 +0200

    with module name

--- modules/fuzzy-edit/default.nix
+++ modules/fuzzy-edit/default.nix
@@ -39,11 +39,13 @@
                 config,
                 pkgs,
                 ...
-            }: nixos @ {...}: {
+            }: nixos @ {...}:
+                let cfg = perSystem.config.nixosModules.fuzzy-edit; in
+            {
                 # services.foo.package = perSystem.config.packages.foo;
 
                 config = {
-                    environment.systemPackages = lib.mkIf perSystem.config.programs.rgv.enable [
+                    environment.systemPackages = lib.mkIf cfg.programs.rgv.enable [
                         perSystem.config.packages.ripgrep-fuzzy-edit
                     ];
                 };

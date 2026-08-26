8415c9910a05f1d1d73c4337ec0930cadab70abb
heureka-code <>
2026-08-26 09:45:06 +0200

    add excludeGlobs option

--- modules/fuzzy-edit/options.nix
+++ modules/fuzzy-edit/options.nix
@@ -43,6 +43,11 @@
                     description = "The command to run as editor";
                     default = "$EDITOR";
                 };
+                excludeGlobs = lib.mkOption {
+                    type = lib.types.listOf lib.types.str;
+                    description = "A list of globs that should be used for '-E ???' flags to fd";
+                    default = ["*.pdf" "*.PDF" "*.mp4" "*.mp3" "*.odt" "*.docx"];
+                };
                 #package = lib.mkOption {
                 #defaultText = lib.literalMD "`packages.default` from the foo flake";
                 #};

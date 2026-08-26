6bd6af532e586bb3510434b9b8bb05b9e99017f3
heureka-code <>
2026-08-26 09:36:46 +0200

    maybe this?

--- modules/fuzzy-edit/config.nix
+++ modules/fuzzy-edit/config.nix
@@ -8,19 +8,18 @@
         config,
         ...
     }: let
-        cfg.rgv = config.programs.rg-fuzzy-edit;
-        cfg.fdv = config.programs.fd-fuzzy-edit;
+        cfg = config.programs;
     in {
-        environment.systemPackages = lib.mkIf cfg.rgv.enable [
+        environment.systemPackages = lib.mkIf cfg.rg-fuzzy-edit.enable [
             (self.packages.${pkgs.stdenv.hostPlatform.system}.rg-fuzzy-edit.override {
-                name = cfg.rgv.name;
-                help = cfg.rgv.help;
-                editor = cfg.rgv.editor;
+                name = cfg.rg-fuzzy-edit.name;
+                help = cfg.rg-fuzzy-edit.help;
+                editor = cfg.rg-fuzzy-edit.editor;
             })
             (self.packages.${pkgs.stdenv.hostPlatform.system}.fd-fuzzy-edit.override {
-                name = cfg.fdv.name;
-                help = cfg.fdv.help;
-                editor = cfg.fdv.editor;
+                name = cfg.fd-fuzzy-edit.name;
+                help = cfg.fd-fuzzy-edit.help;
+                editor = cfg.fd-fuzzy-edit.editor;
             })
         ];
     };
